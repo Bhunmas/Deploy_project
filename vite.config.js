922bfb31c2837792056f4e6a2c88fd55b4e1a46f
Bhunmas <lovebhunma@outlook.com>
2024-11-18 17:22:08 +0700

    docker

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/FrontEnd_POS_Project',
-  server:{
-    host:'127.0.0.1',
-    port:4432
+  base:'/',
+  server: {
+    // watch: {
+    //  usePolling: true,
+    // },
+    host: true, // Here
+    port:5000
+    // strictPort: true,
+ 
   }
+
 })
